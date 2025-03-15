93378b3d6043a792f305cbde9934b7b3d4e16d1a
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-15 13:09:27 +0800

    Serve http with server.js

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,37 @@
+import * as fs from "fs";
+import * as http from "http";
+import * as mime from "mime-types";
+import * as path from "path";
+
 import { WebSocket, WebSocketServer } from "ws";
 import { v4 as uuid } from "uuid";
 
 import { Client, InitMessage } from "./common/types";
 
+const HTTP_PORT = Number(process.env.HTTP_PORT ?? 3000);
+const WS_PORT = Number(process.env.WS_PORT ?? 3001);
+console.debug({ HTTP_PORT, WS_PORT });
+
+http.createServer(async (req, res) => {
+	const urlpath = req.url.split("?")[0];
+	const filepath = path.join(".", "public", urlpath === "/" ? "index.html" : urlpath);
+	const stats = await fs.promises.stat(filepath);
+
+	if (!stats.isFile()) {
+		res.statusCode = 404;
+		res.end('File not found');
+		return;
+	}
+
+	const contentType = mime.contentType(path.extname(filepath)) || 'application/octet-stream';
+	res.setHeader('Content-Type', contentType);
+	fs.createReadStream(filepath).pipe(res);
+}).listen(HTTP_PORT);
+
 const clients: Record<string, Client> = {};
 const sockets: Record<string, WebSocket> = {};
 
-const server = new WebSocketServer({ port: 3001 });
+const server = new WebSocketServer({ port: WS_PORT });
 
 server.on("connection", (socket) => {
 	socket.on("open", () => {

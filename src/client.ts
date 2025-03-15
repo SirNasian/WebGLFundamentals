37713adfb3e84601289187518049647da1bef931
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-15 10:58:36 +0800

    Factor out `shader.js`

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,5 @@
+import { constructShader } from "./shader";
+
 const canvas = document.getElementsByTagName("canvas")[0];
 const gl = (canvas as HTMLCanvasElement).getContext("webgl");
 gl.viewport(0, 0, canvas.width, canvas.height);
@@ -20,53 +22,45 @@ const walls = new Uint8Array([
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 ]);
 
-const program = gl.createProgram();
-const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+const shader = constructShader(
+	gl,
+	`
+		attribute vec2 vertex_position;
+		attribute vec2 vertex_uv;
+		varying vec2 fragment_position;
+		varying vec2 fragment_uv;
+		void main() {
+			gl_Position = vec4(vertex_position, 0.0, 1.0);
+			fragment_position = vertex_position;
+			fragment_uv = vertex_uv;
+		}
+	`,
+	`
+		precision mediump float;
+		uniform sampler2D wall_texture;
+		uniform vec2 cursor_position;
+		varying vec2 fragment_position;
+		varying vec2 fragment_uv;
+		void main() {
+			float intensity = 1.0 - length(fragment_position - cursor_position);
+			float red = texture2D(wall_texture, fragment_uv).r;
 
-gl.shaderSource(vertex_shader, `
-	attribute vec2 vertex_position;
-	attribute vec2 vertex_uv;
-	varying vec2 fragment_position;
-	varying vec2 fragment_uv;
-	void main() {
-		gl_Position = vec4(vertex_position, 0.0, 1.0);
-		fragment_position = vertex_position;
-		fragment_uv = vertex_uv;
-	}
-`);
+			vec2 cursor_uv = vec2((cursor_position.x + 1.0) / 2.0, (cursor_position.y + 1.0) / 2.0);
+			for (int i = 0; i < 1024; i++)
+				if (texture2D(wall_texture, cursor_uv + ((fragment_uv - cursor_uv) / 1024.0) * float(i)).r > 0.0) {
+					intensity = 0.0;
+					break;
+				}
 
-gl.shaderSource(fragment_shader, `
-	precision mediump float;
-	uniform sampler2D wall_texture;
-	uniform vec2 cursor_position;
-	varying vec2 fragment_position;
-	varying vec2 fragment_uv;
-	void main() {
-		float intensity = 1.0 - length(fragment_position - cursor_position);
-		float red = texture2D(wall_texture, fragment_uv).r;
+			if (red == 0.0)
+				gl_FragColor = vec4(vec3(intensity), 1.0);
+			else
+				gl_FragColor = vec4(red, 0.0, 0.0, 1.0);
+		}
+	`,
+);
 
-		vec2 cursor_uv = vec2((cursor_position.x + 1.0) / 2.0, (cursor_position.y + 1.0) / 2.0);
-		for (int i = 0; i < 1024; i++)
-			if (texture2D(wall_texture, cursor_uv + ((fragment_uv - cursor_uv) / 1024.0) * float(i)).r > 0.0) {
-				intensity = 0.0;
-				break;
-			}
-
-		if (red == 0.0)
-			gl_FragColor = vec4(vec3(intensity), 1.0);
-		else
-			gl_FragColor = vec4(red, 0.0, 0.0, 1.0);
-	}
-`);
-
-gl.compileShader(vertex_shader);
-gl.compileShader(fragment_shader);
-
-gl.attachShader(program, vertex_shader);
-gl.attachShader(program, fragment_shader);
-gl.linkProgram(program);
-gl.useProgram(program);
+shader.use();
 
 const texture = gl.createTexture();
 gl.activeTexture(gl.TEXTURE0);
@@ -79,18 +73,17 @@ const vbo = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-const attribute_location0 = gl.getAttribLocation(program, "vertex_position");
+const attribute_location0 = shader.getAttribLocation("vertex_position");
 gl.enableVertexAttribArray(attribute_location0);
 gl.vertexAttribPointer(attribute_location0, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0 * Float32Array.BYTES_PER_ELEMENT);
 
-const attribute_location1 = gl.getAttribLocation(program, "vertex_uv");
+const attribute_location1 = shader.getAttribLocation("vertex_uv");
 gl.enableVertexAttribArray(attribute_location1);
 gl.vertexAttribPointer(attribute_location1, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
 
-const uniform_location = gl.getUniformLocation(program, "cursor_position");
 const bounds = canvas.getBoundingClientRect();
-document.addEventListener("mousemove", ({ x, y }) => gl.uniform2f(uniform_location, 2.0 * (x/bounds.width) - 1.0, 1.0 - (y/bounds.height) * 2));
-gl.uniform1i(gl.getUniformLocation(program, "wall_texture"), 0);
+document.addEventListener("mousemove", ({ x, y }) => shader.setUniform2fv("cursor_position", new Float32Array([2.0 * (x/bounds.width) - 1.0, 1.0 - (y/bounds.height) * 2])));
+shader.setUniform1i("wall_texture", 0);
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 const render = () => {
---
+++ src/shader.ts
@@ -0,0 +1,75 @@
+export interface Shader {
+	use: () => void;
+	getAttribLocation: (name: string) => GLint;
+	getUniformLocation: (name: string) => WebGLUniformLocation;
+	setUniform1i: (name: string, value: GLint) => void;
+	setUniform2fv: (name: string, value: Float32Array) => void;
+}
+
+export const constructShader = (
+	gl: WebGLRenderingContext,
+	vertex_shader: WebGLShader | string,
+	fragment_shader: WebGLShader | string,
+): Shader | null => {
+	const program = createProgram(gl, vertex_shader, fragment_shader);
+	if (!program) return null;
+
+	const attribute_locations: Record<string, number> = {};
+	const getAttribLocation = (name: string): GLint => {
+		if (attribute_locations[name] === undefined)
+			attribute_locations[name] = gl.getAttribLocation(program, name);
+		return attribute_locations[name];
+	};
+
+	const uniform_locations: Record<string, WebGLUniformLocation | null> = {};
+	const getUniformLocation = (name: string): WebGLUniformLocation | null => {
+		if (uniform_locations[name] === undefined)
+			uniform_locations[name] = gl.getUniformLocation(program, name);
+		return uniform_locations[name];
+	};
+
+	return {
+		use: () => gl.useProgram(program),
+		getAttribLocation,
+		getUniformLocation,
+		setUniform1i: (name, value) => gl.uniform1i(getUniformLocation(name), value),
+		setUniform2fv: (name, value) => gl.uniform2fv(getUniformLocation(name), value),
+	};
+}
+
+const createProgram = (
+	gl: WebGLRenderingContext,
+	vertex_shader: WebGLShader | string,
+	fragment_shader: WebGLShader | string,
+): WebGLProgram | null => {
+	const program = gl.createProgram();
+
+	const vertex_shader_ = typeof(vertex_shader) === "string" ? compileShader(gl, gl.VERTEX_SHADER, vertex_shader) : vertex_shader;
+	const fragment_shader_ = typeof(fragment_shader) === "string" ? compileShader(gl, gl.FRAGMENT_SHADER, fragment_shader) : fragment_shader;
+	if (!vertex_shader_ || !fragment_shader_) return null;
+
+	gl.attachShader(program, vertex_shader_);
+	gl.attachShader(program, fragment_shader_);
+	gl.linkProgram(program);
+
+	const info = gl.getProgramInfoLog(program);
+	if (info) console.error(info);
+
+	return info ? null : program;
+};
+
+export const compileShader = (
+	gl: WebGLRenderingContext,
+	shader_type: GLenum,
+	shader_source: string,
+): WebGLShader | null => {
+	const shader = gl.createShader(shader_type);
+
+	gl.shaderSource(shader, shader_source);
+	gl.compileShader(shader);
+
+	const info = gl.getShaderInfoLog(shader);
+	if (info) console.error(info);
+
+	return info ? null : shader;
+};

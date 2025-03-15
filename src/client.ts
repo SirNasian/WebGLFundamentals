9238068f6da8b191cdf21dcac8de9abfed8d7fdd
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2025-03-15 11:22:27 +0800

    Use webgl2 instead of webgl(1)

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { constructShader } from "./shader";
 
 const canvas = document.getElementsByTagName("canvas")[0];
-const gl = (canvas as HTMLCanvasElement).getContext("webgl");
+const gl = (canvas as HTMLCanvasElement).getContext("webgl2");
 gl.viewport(0, 0, canvas.width, canvas.height);
 
 const vertices = new Float32Array([
@@ -24,38 +24,39 @@ const walls = new Uint8Array([
 
 const shader = constructShader(
 	gl,
-	`
-		attribute vec2 vertex_position;
-		attribute vec2 vertex_uv;
-		varying vec2 fragment_position;
-		varying vec2 fragment_uv;
+	`#version 300 es
+		layout (location = 0) in vec2 vertex_position;
+		layout (location = 1) in vec2 vertex_uv;
+		out vec2 fragment_position;
+		out vec2 fragment_uv;
 		void main() {
 			gl_Position = vec4(vertex_position, 0.0, 1.0);
 			fragment_position = vertex_position;
 			fragment_uv = vertex_uv;
 		}
 	`,
-	`
+	`#version 300 es
 		precision mediump float;
 		uniform sampler2D wall_texture;
 		uniform vec2 cursor_position;
-		varying vec2 fragment_position;
-		varying vec2 fragment_uv;
+		in vec2 fragment_position;
+		in vec2 fragment_uv;
+		out vec4 fragment_colour;
 		void main() {
 			float intensity = 1.0 - length(fragment_position - cursor_position);
-			float red = texture2D(wall_texture, fragment_uv).r;
+			float red = texture(wall_texture, fragment_uv).r;
 
 			vec2 cursor_uv = vec2((cursor_position.x + 1.0) / 2.0, (cursor_position.y + 1.0) / 2.0);
 			for (int i = 0; i < 1024; i++)
-				if (texture2D(wall_texture, cursor_uv + ((fragment_uv - cursor_uv) / 1024.0) * float(i)).r > 0.0) {
+				if (texture(wall_texture, cursor_uv + ((fragment_uv - cursor_uv) / 1024.0) * float(i)).r > 0.0) {
 					intensity = 0.0;
 					break;
 				}
 
 			if (red == 0.0)
-				gl_FragColor = vec4(vec3(intensity), 1.0);
+				fragment_colour = vec4(vec3(intensity), 1.0);
 			else
-				gl_FragColor = vec4(red, 0.0, 0.0, 1.0);
+				fragment_colour = vec4(red, 0.0, 0.0, 1.0);
 		}
 	`,
 );
@@ -73,13 +74,11 @@ const vbo = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-const attribute_location0 = shader.getAttribLocation("vertex_position");
-gl.enableVertexAttribArray(attribute_location0);
-gl.vertexAttribPointer(attribute_location0, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0 * Float32Array.BYTES_PER_ELEMENT);
+gl.enableVertexAttribArray(0);
+gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 0 * Float32Array.BYTES_PER_ELEMENT);
 
-const attribute_location1 = shader.getAttribLocation("vertex_uv");
-gl.enableVertexAttribArray(attribute_location1);
-gl.vertexAttribPointer(attribute_location1, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+gl.enableVertexAttribArray(1);
+gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 4 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
 
 const bounds = canvas.getBoundingClientRect();
 document.addEventListener("mousemove", ({ x, y }) => shader.setUniform2fv("cursor_position", new Float32Array([2.0 * (x/bounds.width) - 1.0, 1.0 - (y/bounds.height) * 2])));
